07d3e72c2148d38456ddd49e4365a2b2ffe37982
Biplab Adhikary <55290917+biplabad@users.noreply.github.com>
2023-06-25 17:54:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,7 @@ from urllib.error import URLError
 
 
 # streamlit.title("My Parents New Healthy Dinner");
-
 # streamlit.header("Breakfast");
-
 # streamlit.text("🥣 Soup");
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
@@ -17,7 +15,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Lemon','Banana'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
@@ -55,8 +52,18 @@ if streamlit.button('Get Fruit Load List'):
 
 # my_data_row = my_cur.fetchone()
 # dont run anything past this point while trouble shooting
-streamlit.stop()
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+# streamlit.stop()
 
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+   with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+    return 'Thanks for adding ' + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back from function)
+
+
